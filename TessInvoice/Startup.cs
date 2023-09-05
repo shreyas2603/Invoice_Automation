03e45161572243afbc41960bc2f95ff76f10a913
shreyas2603 <131783157+shreyas2603@users.noreply.github.com>
2023-09-05 19:28:55 +0530

    Add files via upload

---
+++ TessInvoice/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace TessInvoice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme; // Add this line
+
+            })
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/Invoice/Login";
+                options.AccessDeniedPath = "/Invoice/Login"; // Optional, in case of access denied
+            });
+
+
+
+            var uploadFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Invoice");
+            if (!Directory.Exists(uploadFolderPath))
+            {
+                Directory.CreateDirectory(uploadFolderPath);
+            }
+
+            services.AddMvc();
+
+            // Add other services if needed
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Invoice}/{action=ExtractData}/{id?}");
+            });
+            
+
+        }
+    }
+}
